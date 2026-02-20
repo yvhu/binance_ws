e3140ec72e5ce9dc1255493e3292dbe6139b113b
szst7976 <chunyu.hu-vendor@yumchina.com>
2026-02-21 01:32:14 +0800

    init

--- src/telegram/__init__.py
+++ src/telegram/__init__.py
@@ -3,6 +3,5 @@ Telegram Communication Module
 """
 
 from .telegram_client import TelegramClient
-from .message_formatter import MessageFormatter
 
-__all__ = ['TelegramClient', 'MessageFormatter']
+__all__ = ['TelegramClient']
